aac35b3b5c4fe71740cd463f0ba00b0b9042a3c5
JBlazingSun <blazings@vip.qq.com>
2020-12-20 17:14:10 +0800

    init

---
+++ src/main/java/com/blazings/meituanopenapi/MeituanOpenApiApplication.java
@@ -0,0 +1,18 @@
+package com.blazings.meituanopenapi;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeituanOpenApiApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeituanOpenApiApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        
+    }
+}
---
+++ src/test/java/com/blazings/meituanopenapi/MeituanOpenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blazings.meituanopenapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeituanOpenApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
